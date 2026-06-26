a989b69c6880c74b090a0ef61e04e3c133d76735
Anjali Thakur <at978739@gmail.com>
2026-06-26 20:36:11 +0530

    Update verify_data.py

--- verify_data.py
+++ verify_data.py
@@ -3,7 +3,7 @@ import mysql.connector
 conn = mysql.connector.connect(
     host="127.0.0.1",
     user="root",
-    password="anjali@2003",
+    password="your pssw",
     database="blinkit_sales_db"
 )
 
