fb0411ded055f39b2a7ea59edb8707f86d883c8b
Anjali Thakur <at978739@gmail.com>
2026-06-26 20:34:54 +0530

    Update create_database.py

--- create_database.py
+++ create_database.py
@@ -4,7 +4,7 @@ import mysql.connector
 connection = mysql.connector.connect(
     host="localhost",
     user="root",
-    password="anjali@2003"
+    password="your pssw"
 )
 
 cursor = connection.cursor()
