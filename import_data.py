93b9bc63d48f8fa8b48b37fb112001309293a244
Anjali Thakur <at978739@gmail.com>
2026-06-26 20:35:49 +0530

    Update MySQL password in import_data.py

--- import_data.py
+++ import_data.py
@@ -12,7 +12,7 @@ df = df.replace({np.nan: None})
 connection = mysql.connector.connect(
     host="localhost",
     user="root",
-    password="anjali@2003",
+    password="your pssw",
     database="blinkit_sales_db"
 )
 
